171fd89b34830161b988a84d72f888021a8d978f
Ties Jan Hefting <hello@tiesjan.com>
2023-02-10 18:14:55 +0100

    Apply Black & isort style checks to setup.py

--- setup.py
+++ setup.py
@@ -1,47 +1,43 @@
 import setuptools
 
-
-with open('README.md', 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
 
 setuptools.setup(
     # Metadata
-    name='django-convenient-formsets',
-    version='1.2.1',
-    license='BSD',
-    author='Ties Jan Hefting',
-    author_email='hello@tiesjan.com',
-    description='Django dynamic formsets made convenient for users and developers alike.',
+    name="django-convenient-formsets",
+    version="1.2.1",
+    license="BSD",
+    author="Ties Jan Hefting",
+    author_email="hello@tiesjan.com",
+    description="Django dynamic formsets made convenient for users and developers alike.",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    url='https://github.com/tiesjan/django-convenient-formsets',
+    long_description_content_type="text/markdown",
+    url="https://github.com/tiesjan/django-convenient-formsets",
     project_urls={
-        'Bugs': 'https://github.com/tiesjan/django-convenient-formsets/issues',
-        'Changes': 'https://github.com/tiesjan/django-convenient-formsets/blob/main/CHANGES.md'
+        "Bugs": "https://github.com/tiesjan/django-convenient-formsets/issues",
+        "Changes": "https://github.com/tiesjan/django-convenient-formsets/blob/main/CHANGES.md",
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django :: 4.0',
-        'Framework :: Django :: 4.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Internet :: WWW/HTTP',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
     ],
-
     # Options
-    packages=setuptools.find_packages(exclude=('py_tests*',)),
+    packages=setuptools.find_packages(exclude=("py_tests*",)),
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.7',
-    install_requires=(
-        'Django>=4.0',
-    )
+    python_requires=">=3.7",
+    install_requires=("Django>=4.0",),
 )
